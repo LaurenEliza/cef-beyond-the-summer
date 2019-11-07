b95fcf54f9216e4ddba77d77a36f2888dd333131
Lauren Grinder <cardwellauren@gmail.com>
2019-11-07 15:30:32 -0700

    created pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'profile', loadChildren: './pages/profile/profile.module#ProfilePageModule' },
+  { path: 'reset-password', loadChildren: './pages/reset-password/reset-password.module#ResetPasswordPageModule' },
+  { path: 'signup', loadChildren: './pages/signup/signup.module#SignupPageModule' },
+  { path: 'chat', loadChildren: './pages/chat/chat.module#ChatPageModule' },
+  { path: 'prayer-requests', loadChildren: './pages/prayer-requests/prayer-requests.module#PrayerRequestsPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/pages/prayer-requests/prayer-requests.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prayer-requests',
+  templateUrl: './prayer-requests.page.html',
+  styleUrls: ['./prayer-requests.page.scss'],
+})
+export class PrayerRequestsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
